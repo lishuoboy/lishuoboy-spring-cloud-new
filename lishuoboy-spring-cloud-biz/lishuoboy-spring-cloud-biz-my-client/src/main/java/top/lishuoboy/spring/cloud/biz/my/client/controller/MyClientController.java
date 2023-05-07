62f0c589516033298e80b5386288fff61559a62a
lishuoboy <lishuoboy@qq.com>
2023-05-08 00:09:48 +0800

    18.【服务配置】【nacos】【动态实时刷新全部实现】@RefreshScope实现动态实时更新。 http://localhost:8081/myClient/nacosConfigTest。 // nacos动态更新配置@RefreshScope注解后取值为null，动态实时刷新无效原因。 https://blog.csdn.net/qq_36961530/article/details/121926596。

--- lishuoboy-spring-cloud-biz/lishuoboy-spring-cloud-biz-my-client/src/main/java/top/lishuoboy/spring/cloud/biz/my/client/controller/MyClientController.java
+++ lishuoboy-spring-cloud-biz/lishuoboy-spring-cloud-biz-my-client/src/main/java/top/lishuoboy/spring/cloud/biz/my/client/controller/MyClientController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +22,8 @@ import top.lishuoboy.spring.cloud.biz.my.server.api.service.MyServerApi;
 import java.net.URI;
 import java.util.List;
 
+// 默认为 ScopedProxyMode.TARGET_CLASS,无法动态实时刷新nacos参数，切导致为null，改为 ScopedProxyMode.DEFAULT
+@RefreshScope(proxyMode = ScopedProxyMode.DEFAULT)
 @Slf4j
 @RestController
 @RequestMapping("/myClient")
@@ -56,10 +60,10 @@ public class MyClientController {
     }
 
 
-    /** 对于@Value。nacos修改时，若需自动刷新，需使用 @RefreshScope */
+    /** 对于@Value。nacos修改时，若需自动动态实时刷新，需使用 @RefreshScope */
     @Value("${my.nickName:小石头}")
     private String nickName;
-    /** 对于@ConfigurationProperties。 nacos修改时，会自动刷新，无需使用 @RefreshScope */
+    /** 对于@ConfigurationProperties。 nacos修改时，会自动动态实时刷新，无需使用 @RefreshScope */
     @Autowired
     private MyClientNacosProp nacosProp;
 
