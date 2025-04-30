b309dbb1bf661fbf5f62ae7bd0714a5d893503c6
lishuoboy <lishuoboy@qq.com>
2025-04-30 16:06:25 +0800

    12.【服务调用】【openFeign】【自动负载均衡策略】补充报错解决方案。多个@FeignClient会导致重复注入spring报错，通过contextId制定不同的beanName

--- lishuoboy-spring-cloud-biz/lishuoboy-spring-cloud-biz-my-server-api/src/main/java/top/lishuoboy/spring/cloud/biz/my/server/api/service/MyServerApi.java
+++ lishuoboy-spring-cloud-biz/lishuoboy-spring-cloud-biz-my-server-api/src/main/java/top/lishuoboy/spring/cloud/biz/my/server/api/service/MyServerApi.java
@@ -8,7 +8,10 @@ import top.lishuoboy.spring.cloud.biz.my.server.api.dict.ServiceInfoMyServer;
 import top.lishuoboy.spring.cloud.biz.my.server.api.entity.Cat;
 
 @Service
-@FeignClient(value = ServiceInfoMyServer.NAME, path = "/myServer")  // 大小写敏感
+/// 【注意】contextId 用于配置 注入spring 的 beanName，防止beanName重复报错。
+/// 当配置多个 “@FeignClient(value = ServiceInfoMyServer.NAME” 且未指定contextId时，
+/// 会报错“The bean 'myServerApi.FeignClientSpecification' could not be registered. A bean with that name has already been defined and overriding is disabled.”。
+@FeignClient(value = ServiceInfoMyServer.NAME, path = "/myServer", contextId = "myServerApi")  // 大小写敏感
 public interface MyServerApi {
     @GetMapping("/openFeignTest")
     Cat openFeignTest(@RequestParam String name);
